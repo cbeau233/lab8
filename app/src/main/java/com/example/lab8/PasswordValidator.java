4d0c0d74de4dac569fd23285cc278f5eac971c73
cbaue233 <cbeau233@uottawa.ca>
2024-11-21 13:39:13 -0500

    Password Validation

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -1,4 +1,6 @@
 package com.example.lab8;
+import java.util.Arrays;
+import java.util.List;
 
 public class PasswordValidator {
 
@@ -7,13 +9,24 @@ public class PasswordValidator {
 
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
+        boolean hasNumber = false;
+        Character[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        List<Character> numberList = Arrays.asList(numbers);
+
+
+
+        if (password.strip().length() < 10) {
+            return false;
+        }
+
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (numberList.contains(c)) hasNumber = true;
 
             // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+            if (hasUpperCase && hasLowerCase && hasNumber) return true;
         }
         return false;
     }
